e52db2bf239bd4b2d900fe31565f7927a3de2735
Matt Brightman <mwb2017@gmail.com>
2020-06-26 11:03:25 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def git_test(a,b):
+    c = ((a*a)+(b*b))**(.5)
+    return float(c)
+
+a = int(input("First Number: "))
+b = int(input("Second Number: "))
+print("The hypontenuse is: ", git_test(a,b))
+
