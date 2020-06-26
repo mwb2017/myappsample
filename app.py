ae31ce4f67d3c70206e484980d4b79d8e81d9d8a
Matt Brightman <mwb2017@gmail.com>
2020-06-26 11:41:42 -0500

    iter1 commit

--- app.py
+++
@@ -1,8 +0,0 @@
-def git_test(a,b):
-    c = ((a*a)+(b*b))**(.5)
-    return float(c)
-
-a = int(input("First Number: "))
-b = int(input("Second Number: "))
-print("The hypontenuse: ", git_test(a,b))
-
